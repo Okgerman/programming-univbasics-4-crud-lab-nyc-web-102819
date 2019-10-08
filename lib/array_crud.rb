7d833ece143223001b4cf6f3acf35fabd24535fc
Joseph Stalnaker <josephlstalnaker@gmail.com>
2019-10-08 03:44:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  remove_element_from_start_of_array.shift
+  p remove_element_from_start_of_array
 end
 
 def retrieve_element_from_index(array, index_number)
