12ce15f73cb06f96908ea3ec942ed3e556940f2c
Joseph Stalnaker <josephlstalnaker@gmail.com>
2019-10-08 03:35:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,7 @@ def create_an_array
   [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
+def add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
   add_element_to_end_of_array.last "arrays!"
 end
 
