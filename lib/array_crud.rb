5c0bdf3bceb6c5339d63083aae14ac6acc82f4a1
Joseph Stalnaker <josephlstalnaker@gmail.com>
2019-10-08 03:21:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  element = "wow"
+  add_element_to_end_of_array << "arrays!"
   p add_element_to_end_of_array
 end
 
